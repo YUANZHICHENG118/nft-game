f344d6fdd9dd8aff367fbd67a25a85195b426dea
金光闪闪亮 <75668714@qq.com>
2020-12-28 23:36:00 +0800

    语言配置

---
+++ src/Langue.ts
@@ -0,0 +1,16 @@
+export default class Langue extends  Laya.EventDispatcher {
+    private static cn={
+        ok:'确定',
+        close:'关闭'
+    }
+    private static en={
+        ok:'ok',
+        close:'close'
+    }
+    private static xxx={
+        ok:'确定',
+        close:'关闭'
+    }
+    constructor() { super(); }
+    
+}
