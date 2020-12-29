73129c06345665161c24b0c03a380669f1615bb9
yuanzhicheng <313640087@qq.com>
2020-12-29 10:31:59 +0800

    init

--- src/Langue.ts
+++ src/Langue.ts
@@ -1,16 +1,116 @@
-export default class Langue extends  Laya.EventDispatcher {
-    private static cn={
-        ok:'确定',
-        close:'关闭'
+export default class Langue extends Laya.EventDispatcher {
+    private static cn = {
+        ok: '确定',
+        close: '关闭',
+        notice: '公告',
+        email: '邮件',
+        chat: '聊天',
+        help: '帮助',
+        home_0: '我知道了',
+        home_1: '一键领取',
+        home_2: '一键读取',
+        nav1: '设备',
+        nav2: '交易',
+        nav3: '排行',
+        nav4: '我的',
+        nav1_1: '低',
+        nav1_2: '高',
+        nav1_3: '全选',
+        nav1_4: '自动匹配',
+        nav1_5: '总载重',
+        nav1_6: '总挖矿',
+        nav1_7: '预计收益',
+        nav1_8: '开始挖矿',
+        nav3_1: '当前',
+        nav3_2: '全网',
+        nav3_3: '排名',
+        nav3_4: '账号',
+        nav3_5: '运走量',
+        nav4_1: '基本信息',
+        nav4_2: '我的收益',
+        nav4_3: '返佣明细',
+        nav4_4: '昵称',
+        nav4_5: '我的地址',
+        nav4_6: '钱包',
+        nav4_7: '邀请链接',
+        nav4_8: '复制'
+
     }
-    private static en={
-        ok:'ok',
-        close:'close'
+    private static en = {
+        ok: 'OK',
+        close: 'Close',
+        notice: 'Announcement',
+        email: 'Mail',
+        chat: 'Chat',
+        help: 'Help',
+        home_0: 'I got it',
+        home_1: 'One key to receive',
+        home_2: 'One-key reading',
+        nav1: 'device',
+        nav2: 'transaction',
+        nav3: 'ranking',
+        nav4: 'mine',
+        nav1_1: 'low',
+        nav1_2: 'High',
+        nav1_3: 'Select all',
+        nav1_4: 'Automatic matching',
+        nav1_5: 'total load',
+        nav1_6: 'Total mining',
+        nav1_7: 'Estimated income',
+        nav1_8: 'Start mining',
+        nav3_1: 'current',
+        nav3_2: 'full network',
+        nav3_3: 'rank',
+        nav3_4: 'Account',
+        nav3_5: 'shipping volume',
+        nav4_1: 'Basic Information',
+        nav4_2: 'My earnings',
+        nav4_3: 'Rebate details',
+        nav4_4: 'nickname',
+        nav4_5: 'My address',
+        nav4_6: 'Wallet',
+        nav4_7: 'Invitation link',
+        nav4_8: 'copy'
     }
-    private static xxx={
-        ok:'确定',
-        close:'关闭'
+    private static kr = {
+        ok: '확인',
+        close: '닫기',
+        notice: '발표',
+        email: '우편',
+        chat: '채팅',
+        help: '도움말',
+        home_0: '알았어요',
+        home_1: '수신 할 하나의 열쇠',
+        home_2: '원키 읽기',
+        nav1: '장치',
+        nav2: '트랜잭션',
+        nav3: '순위',
+        nav4: '나의 것',
+        nav1_1: '낮은',
+        nav1_2: '높음',
+        nav1_3: '모두 선택',
+        nav1_4: '자동 일치',
+        nav1_5: '총 부하',
+        nav1_6: '총 채굴',
+        nav1_7: '예상 수입',
+        nav1_8: '채굴 시작',
+        nav3_1: '현재',
+        nav3_2: '전체 네트워크',
+        nav3_3: '순위',
+        nav3_4: '계정',
+        nav3_5: '배송량',
+        nav4_1: '기본 정보',
+        nav4_2: '내 수입',
+        nav4_3: '환급 내역',
+        nav4_4: '별명',
+        nav4_5: '내 주소',
+        nav4_6: '지갑',
+        nav4_7: '초대 링크',
+        nav4_8: '복사'
     }
-    constructor() { super(); }
-    
+
+    constructor() {
+        super();
+    }
+
 }
